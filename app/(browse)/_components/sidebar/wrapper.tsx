640b652c0144ff878b713a6589ed793859483dad
devopsdbroe <dbroe1995@gmail.com>
2023-12-22 20:56:22 -0500

    Implement follow list in sidebar

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -4,8 +4,10 @@ import { useIsClient } from "usehooks-ts";
 
 import { cn } from "@/lib/utils";
 import { useSidebar } from "@/store/use-sidebar";
+
 import { ToggleSkeleton } from "./toggle";
 import { RecommendedSkeleton } from "./recommended";
+import { FollowingSkeleton } from "./following";
 
 interface WrapperProps {
 	children: React.ReactNode;
@@ -19,6 +21,7 @@ export const Wrapper = ({ children }: WrapperProps) => {
 		return (
 			<aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50">
 				<ToggleSkeleton />
+				<FollowingSkeleton />
 				<RecommendedSkeleton />
 			</aside>
 		);
