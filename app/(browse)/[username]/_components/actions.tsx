8cd55877c9d9554ddfd13c69050cd29820b585b3
devopsdbroe <dbroe1995@gmail.com>
2023-12-22 22:53:06 -0500

    Add ability to block users

--- app/(browse)/[username]/_components/actions.tsx
+++ app/(browse)/[username]/_components/actions.tsx
@@ -5,6 +5,7 @@ import { useTransition } from "react";
 import { onFollow, onUnfollow } from "@/actions/follow";
 import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
+import { onBlock, onUnblock } from "@/actions/block";
 
 interface ActionsProps {
 	isFollowing: boolean;
@@ -42,13 +43,31 @@ export const Actions = ({ isFollowing, userId }: ActionsProps) => {
 		}
 	};
 
+	const handleBlock = () => {
+		startTransition(() => {
+			onUnblock(userId)
+				.then((data) =>
+					toast.success(`Blocked the user ${data.blocked.username}`)
+				)
+				.catch(() => toast.error("Something went wrong"));
+		});
+	};
+
 	return (
-		<Button
-			disabled={isPending}
-			onClick={onClick}
-			variant="primary"
-		>
-			{isFollowing ? "Unfollow" : "Follow"}
-		</Button>
+		<>
+			<Button
+				disabled={isPending}
+				onClick={onClick}
+				variant="primary"
+			>
+				{isFollowing ? "Unfollow" : "Follow"}
+			</Button>
+			<Button
+				disabled={isPending}
+				onClick={handleBlock}
+			>
+				Block
+			</Button>
+		</>
 	);
 };
--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -1,6 +1,9 @@
-import { isFollowingUser } from "@/lib/follow-service";
-import { getUserByUsername } from "@/lib/user-service";
 import { notFound } from "next/navigation";
+
+import { getUserByUsername } from "@/lib/user-service";
+import { isFollowingUser } from "@/lib/follow-service";
+import { isBlockedByUser } from "@/lib/block-service";
+
 import { Actions } from "./_components/actions";
 
 interface UserPageProps {
@@ -17,12 +20,14 @@ const UserPage = async ({ params }: UserPageProps) => {
 	}
 
 	const isFollowing = await isFollowingUser(user.id);
+	const isBlocked = await isBlockedByUser(user.id);
 
 	return (
 		<div className="flex flex-col gap-y-4">
 			<p>Username: {user.username}</p>
 			<p>user Id: {user.id}</p>
 			<p>Is following: {`${isFollowing}`}</p>
+			<p>Is blocked by this user: {`${isBlocked}`}</p>
 			<Actions
 				isFollowing={isFollowing}
 				userId={user.id}
