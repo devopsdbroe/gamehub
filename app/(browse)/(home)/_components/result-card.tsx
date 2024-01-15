faa22affd99c50c222418896b7a413f439efc385
devopsdbroe <dbroe1995@gmail.com>
2024-01-14 21:50:46 -0500

    Bug fix for live badge component not showing

--- app/(browse)/(home)/_components/result-card.tsx
+++ app/(browse)/(home)/_components/result-card.tsx
@@ -1,9 +1,8 @@
 import Link from "next/link";
-import { Stream, User } from "@prisma/client";
+import { User } from "@prisma/client";
 
 import { Skeleton } from "@/components/ui/skeleton";
 import { Thumbnail, ThumbnailSkeleton } from "@/components/thumbnail";
-import { LiveBadge } from "@/components/live-badge";
 import { UserAvatar, UserAvatarSkeleton } from "@/components/user-avatar";
 
 interface ResultCardProps {
@@ -25,11 +24,6 @@ export const ResultCard = ({ data }: ResultCardProps) => {
 					isLive={data.isLive}
 					username={data.user.username}
 				/>
-				{data.isLive && (
-					<div className="absolute top-2 left-2 group-hover:translate-x-2 group-hover:-translate-y-2 transition-transform">
-						<LiveBadge />
-					</div>
-				)}
 				<div className="flex gap-x-3">
 					<UserAvatar
 						username={data.user.username}
