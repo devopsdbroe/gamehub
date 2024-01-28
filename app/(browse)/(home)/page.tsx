ce75d295a08a4cb76d823d401caaa1ec4bc41961
devopsdbroe <dbroe1995@gmail.com>
2024-01-28 17:05:12 -0500

    Update dependencies + fix import typo

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from "react";
-import { Results, ResultsSkeleton } from "./_components/Results";
+import { Results, ResultsSkeleton } from "./_components/results";
 
 export default function Home() {
 	return (
